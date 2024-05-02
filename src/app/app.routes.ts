e68d93d756ae0c426b5fd625420db4db8fcac743
Maxime De Sogus <desogus.maxime@moviplus.ch>
2024-05-02 09:29:08 +0200

    remove useless cmp
    add redirection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,7 @@ import { Routes } from '@angular/router';
 import { DeparturesComponent } from './departures/departures.component';
 
 export const routes: Routes = [
-    { path: 'departures', component: DeparturesComponent }
+    { path: 'departures', component: DeparturesComponent },
+    { path: '', redirectTo: 'departures', pathMatch: 'full' },
+    { path: '**', redirectTo: 'departures' },
 ];
